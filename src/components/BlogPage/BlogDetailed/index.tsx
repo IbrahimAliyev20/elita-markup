bfb58e223ba675598cf069136cf283df988700e2
IbrahimAliyev20 <iboisa12@gmail.com>
2025-07-22 13:32:57 +0400

    elita

--- src/components/BlogPage/BlogDetailed/index.tsx
+++ src/components/BlogPage/BlogDetailed/index.tsx
@@ -56,40 +56,43 @@ function BlogDetails({ blog }: BlogDetailsProps) {
         priority
       />
 
-      <p className="text-elementSecondaryDate text-base font-normal leading-6 text-right">
-        {dateString}
-      </p>
-
       <div
         className="text-elementSecondaryDate text-base font-normal leading-6"
         dangerouslySetInnerHTML={{ __html: blog.description }}
       />
 
-      <div className="flex gap-5 items-center justify-end ">
-        <p className="text-elementSecondaryDate text-lg font-normal leading-6">
-          {t("share")} :
-        </p>
-        <button
-          onClick={() => handleSocialShare("instagram")}
-          aria-label="Share on Instagram"
-          className="hover:opacity-80 transition-opacity cursor-pointer"
-        >
-          <LuInstagram className="text-2xl text-neutral-800" />
-        </button>
-        <button
-          onClick={() => handleSocialShare("facebook")}
-          aria-label="Share on Facebook"
-          className="hover:opacity-80 transition-opacity cursor-pointer"
-        >
-          <FaFacebookF className="text-2xl text-neutral-800" />
-        </button>
-        <button
-          onClick={() => handleSocialShare("linkedin")}
-          aria-label="Share on LinkedIn"
-          className="hover:opacity-80 transition-opacity cursor-pointer"
-        >
-          <FaLinkedinIn className="text-2xl text-neutral-800" />
-        </button>
+      <div className="flex w-full gap-5 justify-between border-t-[1px] border-elementSecondaryDate/20 pt-5"> 
+        <div className="">
+          <p className="text-elementSecondaryDate text-base font-bold leading-6 text-right">
+            {dateString}
+          </p>
+        </div>
+        <div className="flex items-center gap-3">
+          <p className="text-elementSecondaryDate text-lg font-bold leading-6">
+            {t("share")} :
+          </p>
+          <button
+            onClick={() => handleSocialShare("instagram")}
+            aria-label="Share on Instagram"
+            className="hover:opacity-80 transition-opacity cursor-pointer"
+          >
+            <LuInstagram className="text-2xl text-neutral-800" />
+          </button>
+          <button
+            onClick={() => handleSocialShare("facebook")}
+            aria-label="Share on Facebook"
+            className="hover:opacity-80 transition-opacity cursor-pointer"
+          >
+            <FaFacebookF className="text-2xl text-neutral-800" />
+          </button>
+          <button
+            onClick={() => handleSocialShare("linkedin")}
+            aria-label="Share on LinkedIn"
+            className="hover:opacity-80 transition-opacity cursor-pointer"
+          >
+            <FaLinkedinIn className="text-2xl text-neutral-800" />
+          </button>
+        </div>
       </div>
     </div>
   );
